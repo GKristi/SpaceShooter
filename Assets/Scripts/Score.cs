1dfcee1b478dc8b25398e0fdfec9d851aaef4e13
GKristi <christigolub@gmail.com>
2017-05-28 14:56:25 +0300

    rename

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public static int score;
+    private Text text; 
+  
+    void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update() {
+        text.text = "Score: " + score;
+    }
+
+    void Hit()
+    {
+        score++;
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    // Variable to store the enemy prefab
+    public GameObject enemy;
+    // Variable to know how fast we should create new enemies
+    public float spawnTime = 2;
+
+
+	void Start () {
+
+        InvokeRepeating("AddEnemy", 0, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AddEnemy() {
+
+        Renderer rd = GetComponent<Renderer>();
+
+        float x1 = transform.position.x - rd.bounds.size.x / 2;
+        float x2 = transform.position.x + rd.bounds.size.x / 2;
+
+        Vector2 spawnPoint = new Vector2(Random.Range(x1, x2), transform.position.y);
+
+        // Create an enemy at the 'spawnPoint' position
+        Instantiate(enemy, spawnPoint, Quaternion.identity);
+    }
+}
